c4ed5da662dc84aff63e442af45a6385bfa74e5f
MR.SADIYA OFC <sadiyaofc44@gmail.com>
2024-08-31 15:43:47 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "null", // මෙතනට SESSION_ID එක දෙන්න
-MONGODB: process.env.MONGODB || "nul", // mongodb url එක දෙන්න
+SESSION_ID: process.env.SESSION_ID || "", // මෙතනට SESSION_ID එක දෙන්න
+MONGODB: process.env.MONGODB || "", // mongodb url එක දෙන්න
 };
