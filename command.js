e65546049a1c3d40aed26995554e2e479e7856ea
MR.SADIYA OFC <sadiyaofc44@gmail.com>
2024-09-17 12:53:31 +0530

    Update command.js

--- command.js
+++ command.js
@@ -5,7 +5,7 @@
 
    * @project_name : SADIYA-MD
    * @authors : SADIYA-MD(sadiya) <https://github.com/Sadiya70/SADIYA-MD>
-   * @youtube : https://www.youtube.com/@SuhailTechInfo
+   * @youtube : https://www.youtube.com/@sadiyaofficial44
    * @description : SADIYA-MD ,A Multi-functional whatsapp user bot.
    * @version 1.5.0
 
