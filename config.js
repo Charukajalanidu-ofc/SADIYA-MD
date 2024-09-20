65bc629bd84e20ae1537551000b2bcc7a85ac83a
Charukajalanidu-ofc <154059188+Charukajalanidu-ofc@users.noreply.github.com>
2024-09-20 15:34:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "0msmTJrD#teQpWm4dYfiWwwRve1eLSYahclmtK79OgFGgBJ4JAcA", // මෙතනට SESSION_ID එක දෙන්න
+SESSION_ID: process.env.SESSION_ID || "vTRA1KaI#86MM2_avhP3jbV1lacGqEZbtjOxKz_ZDoyJ6ycqadTg", // මෙතනට SESSION_ID එක දෙන්න
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net", // mongodb url එක දෙන්න
 };
